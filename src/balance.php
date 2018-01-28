5f0f0e5383b6aee074b30c08181fa7086ea48e03
Vladimir Beloplotov <cgartscorp@gmail.com>
2018-01-29 00:15:43 +0500

    Night commit2

--- src/balance.php
+++ src/balance.php
@@ -3,47 +3,45 @@ namespace Beloplotov;
 
 class Balance
 {
-   public function balanceBrackets($string)
-  {
-      try {
-          preg_match_all('/[^()\r\s\n\t]/', $string, $value, PREG_SET_ORDER, 0);
-          if (!empty($value)) {
-              throw new \InvalidArgumentException('Недопустимые символы');
-          }
-          $arrayString = str_split($string);
+    public function balanceBrackets($string)
+    {
+        try {
+            preg_match_all('/[^()0-9\r\n\t\s]/', $string, $value, PREG_SET_ORDER, 0);
+            if (!empty($value)) {
+                throw new \InvalidArgumentException('Недопустимые символы');
+            }
+            $arrayString = str_split($string);
 
             $amountBrackets=0;
-          for ($i = 0;
-               $i <= count($arrayString);
-               $i++) {
-              if ($arrayString[$i] == '(') {
+            for ($i = 0;
+                 $i <= count($arrayString);
+                 $i++) {
+                if (@$arrayString[$i] == "(") {
 
-                  $amountBrackets++;
-              }
+                    $amountBrackets++;
+                }
 
-              if ($arrayString[$i] == ')') {
+                if (@$arrayString[$i] == ")") {
 
-                  $amountBrackets--;
-              }
-              if ($amountBrackets < 0) {
-                  throw new \Exception("Неправильно стоит скобка");
-              }
-          }
-          if ($amountBrackets == 1) {
-              throw new \Exception("Где то не закрыта скобка!");
-          }
-          if ($amountBrackets > 1) {
+                    $amountBrackets--;
+                }
+                if ($amountBrackets < 0) {
+                    throw new \Exception("Неправильно стоит скобка");
+                }
+            }
+            if ($amountBrackets == 1) {
+                throw new \Exception("Где то не закрыта скобка!");
+            }
+            if ($amountBrackets > 1) {
                 throw new \Exception("Где то не закрыты" . $amountBrackets . " скобки");
-          }
-          if ($amountBrackets == 0) {
-            throw new \Exception("Выражение составленно верно, скобки все закрыты!");
-          }
-      }catch (\InvalidArgumentException $e){
-         echo $e->getMessage();
-      }catch (\Exception $e){
-          echo $e->getMessage();
-      }
-      return true;
-  }
+            }
+
+        }catch (\InvalidArgumentException $e){
+            return ['status' => false , 'message' => $e->getMessage()];
+        }catch (\Exception $e){
+            return false;
+        }
+        return true;
+    }
 
 }
