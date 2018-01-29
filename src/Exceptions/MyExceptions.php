e0b4e33887e8529f8c64e281218252300c7900aa
Vladimir Beloplotov <cgartscorp@gmail.com>
2018-01-29 16:44:02 +0500

    commit

--- src/Exceptions/MyExceptions.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: scorp
- * Date: 25.01.18
- * Time: 21:26
- */
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "src/balance.php";
-//require_once __DIR__.'/vendor/autoload.php';
-use Beloplotov\Balance as Balance;
-$formula = "((27 + 38) ÷ (77 – 69 x (54 x (26 - 3)))) x (11 x 12 – 17 + 18) – 36 ÷ (32 – 10 x 4)";
-
-
-$foo = new Balance($formula);
-$foo->proverka();
